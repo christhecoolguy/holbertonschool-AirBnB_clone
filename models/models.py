fced493e9b447a20aa625858701ffda34c82e181
DominickKeeling <dominickckeeling@gmail.com>
2023-06-09 18:34:36 -0700

    added models file

---
+++ models/models.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module for models dict """
+
+
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+
+model_classes = dict({("BaseModel", BaseModel),
+                      ("User", User),
+                      ("State", State),
+                      ("City", City),
+                      ("Place", Place),
+                      ("Amenity", Amenity),
+                      ("Review", Review)})
