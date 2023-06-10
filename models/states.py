b2d199d348a930886bbe96b6a4d19434e5d267d6
christhecoolguy <christopherdiggs1@gmail.com>
2023-06-09 18:16:55 -0700

    states

---
+++ models/states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    states model
+
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+
+    """
+        Info on AirBnB States
+        Public class attributes:
+            name: string - empty string
+    """
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def to_dict(self):
+        return super(State, self).to_dict()
