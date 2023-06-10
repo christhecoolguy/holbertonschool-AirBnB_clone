3ab979b8a46b62961227c91b3abb378614d782e5
christhecoolguy <christopherdiggs1@gmail.com>
2023-06-09 17:49:13 -0700

    city

---
+++ models/city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+cities
+"""
+from modles.base_model import BaseModel
+
+
+class City(BaseModel):
+
+    """
+    info airbnb city 
+    public state id string empty it is state id name empty
+    """
+    state_id = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def to _dict(self):
+        return super (City, self).to_dict()
+    
