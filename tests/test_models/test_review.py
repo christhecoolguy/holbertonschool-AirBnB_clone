1cc16e645b51a48a407d29f937a109c15831d4d3
christhecoolguy <christopherdiggs1@gmail.com>
2023-06-12 10:12:37 -0700

    reivew

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" unit test for review """
+
+import initest
+import os 
+from models.engine.file_storage import FileStorage
+from datetime import datetime
+from models.review import review
+
+
+myreview = Review()
+
+class TestReview(unittest.TestCase):
+    """ Class for Review tests """
+
+    def test_id(self):
+        """ Unittesting test_id instance """
+        self.assertTrue(isinstance(myreview.id, str))
+
+    def test_place_id(self):
+        """ Make sure place_id is string """
+        self.assertTrue(isinstance(Review.place_id, str))
+        self.assertTrue(isinstance(myreview.place_id, str))
+
+    def test_user_id(self):
+        """ Make sure user_id is string """
+        self.assertTrue(isinstance(Review.user_id, str))
+        self.assertTrue(isinstance(myreview.user_id, str))
+
+    def test_text(self):
+        """ Make sure text is string """
+        self.assertTrue(isinstance(Review.text, str))
+        self.assertTrue(isinstance(myreview.text, str))
