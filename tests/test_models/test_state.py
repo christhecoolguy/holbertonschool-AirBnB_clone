f9dd4b9e84f2afeddb779bd52ad1433f26446917
christhecoolguy <christopherdiggs1@gmail.com>
2023-06-12 10:28:36 -0700

    state

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Unit test module for State class """
+
+import unittest
+import os
+from models.engine.file_storage import FileStorage
+from datetime import datetime
+from models.state import State
+
+
+state1 = State()
+
+
+class TestState(unittest.TestCase):
+    """" Test state class """
+
+    def test_id (self)
+     """ unit test_id """
+     self.assertTrue(isinstance(state1.id, str))
+
+     def test_name (self):
+        """ makes sure name is good str """
+        self.assertTrue(isinstance(State.name, str))
+        self.assertTRue(isinsance(state1.name, str))
