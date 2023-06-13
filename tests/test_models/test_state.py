afb3dc9ad3765fe72501f785489f048665fd4de2
DominickKeeling <dominickckeeling@gmail.com>
2023-06-13 13:24:42 -0700

    bug fix

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -14,11 +14,11 @@ state1 = State()
 class TestState(unittest.TestCase):
     """" Test state class """
 
-    def test_id (self)
+    def test_id(self)
      """ unit test_id """
      self.assertTrue(isinstance(state1.id, str))
 
-     def test_name (self):
+     def test_name(self):
         """ makes sure name is good str """
         self.assertTrue(isinstance(State.name, str))
         self.assertTrue(isinstance(state1.name, str))
